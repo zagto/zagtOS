1288ca397ca4bb491a7f8b0a67b5aa89cbf4b5a6
Tobias Zagorni <tobias@zagorni.eu>
2019-07-27 14:24:35 +0200

    virtualbox emulate: handle missing vdi file

--- emulate/virtualbox.sh
+++ emulate/virtualbox.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-rm out/disk.vdi
+rm -f out/disk.vdi
 VBoxManage convertfromraw out/disk.img out/disk.vdi --format VDI --uuid "da082bea-b016-4d9d-a011-93d8992f04a4"
 netcat -l -p 30000 localhost &
 VBoxManage startvm Test
