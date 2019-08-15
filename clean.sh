7348f7e60393800c997b5de6b2ef46f353c60fc4
Tobias Zagorni <tobias@zagorni.eu>
2019-08-15 13:37:02 +0200

    adjust scripts for ACPIHAL rename

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -rf build/acpi/ build/libzagtos/ build/initdata build/musl/ build/headers/ src/musl/obj/ build/initdata/ build/acpi/ build/kernel build/loader build/SystemEnvironment
+rm -rf build/acpi/ build/libzagtos/ build/initdata build/musl/ build/headers/ src/musl/obj/ build/initdata/ build/ACPIHAL build/kernel build/loader build/SystemEnvironment
