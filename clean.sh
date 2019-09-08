e4043ad595b526b8c8c2618c36ec2a6482c65ee2
Tobias Zagorni <tobias@zagorni.eu>
2019-09-08 17:57:41 +0200

    config/clean: build zbon and zagtos++

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -rf build/acpi/ build/libzagtos/ build/initdata build/musl/ build/headers/ src/musl/obj/ build/initdata/ build/ACPIHAL build/kernel build/loader build/SystemEnvironment
+rm -rf build/acpi/ build/libzagtos/ build/initdata build/musl/ build/headers/ src/musl/obj/ build/libstdc++ build/zbon build/zagtos++ build/initdata/ build/ACPIHAL build/kernel build/loader build/SystemEnvironment
--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-MODULES="headers toolchain loader kernel musl libstdc++ ACPIHAL SystemEnvironment"
+MODULES="headers toolchain loader kernel musl libstdc++ zbon zagtos++ ACPIHAL SystemEnvironment"
 export SHBUILD_ROOT="$PWD"
 export PARALLEL_JOBS=4
 export ARCH=x86_64
