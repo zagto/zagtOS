612df6e5b355258b3afaf6eb5e1dbd328fd144dd
Tobias Zagorni <tobias@zagorni.eu>
2019-11-09 12:34:59 +0100

    x86_64 system: fix patching of secondary processor entry code

--- src/kernel/arch/x86_64/system/System.cpp
+++ src/kernel/arch/x86_64/system/System.cpp
@@ -8,18 +8,19 @@ void System::setupSecondaryProcessorEntry(BootInfo *bootInfo) {
     size_t mptOffset = static_cast<size_t>(&SecondaryProcessorEntryMasterPageTable
                                            - &SecondaryProcessorEntryCode);
     size_t mptAddress = bootInfo->masterPageTable.value();
+    size_t entryCodeAddress = secondaryProcessorEntry.value();
 
     assert(mptAddress < 0x100000000);
     assert(length <= PAGE_SIZE);
 
-    cout << "loading secondary processor entry code to " << secondaryProcessorEntry.value() << endl;
+    cout << "loading secondary processor entry code to " << entryCodeAddress << endl;
     cout << "boot MPT at " << mptAddress << endl;
 
     uint8_t *destination = secondaryProcessorEntry.identityMapped().asPointer<uint8_t>();
     memcpy(destination, &SecondaryProcessorEntryCode, length);
 
     /* put the address the entry code is loaded to at offset 2 (immediate operand of mov) */
-    memcpy(destination + 2, &mptAddress, sizeof(uint32_t));
+    memcpy(destination + 2, &entryCodeAddress, sizeof(uint32_t));
     memcpy(destination + mptOffset, &mptAddress, sizeof(uint32_t));
 
     /* identity-map the entry code so it continues to work when it enables paging */
