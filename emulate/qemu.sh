6f75f897e6447d9956fd803f50439bcbcc5b617b
Tobias Zagorni <tobias@zagorni.eu>
2020-12-21 13:53:34 +0100

    emulate: update qemu and VirtualBox scripts to use DebugBridge

--- emulate/qemu.sh
+++ emulate/qemu.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -e
-
 qemu-system-x86_64 \
     -drive if=pflash,format=raw,unit=0,file=/usr/share/ovmf/x64/OVMF_CODE.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=/usr/share/ovmf/x64/OVMF_VARS.fd,readonly=on \
+    -m 512 \
     -net none \
     -drive id=disk,file=out/disk.img,if=none \
     -device ahci,id=ahci \
     -device ide-drive,drive=disk,bus=ahci.0 \
-    -serial stdio \
     -smp 2 \
+    -serial telnet:localhost:30000,server \
+    -machine q35 \
     -no-reboot \
- -no-reboot -no-shutdown   -d guest_errors,cpu_reset \
-    -s
-
+     -no-reboot -no-shutdown   -d int,guest_errors,cpu_reset \
+    -s &
+build/DebugBridge/DebugBridge
--- emulate/virtualbox.sh
+++ emulate/virtualbox.sh
@@ -2,5 +2,5 @@
 set -e
 rm -f out/disk.vdi
 VBoxManage convertfromraw out/disk.img out/disk.vdi --format VDI --uuid "da082bea-b016-4d9d-a011-93d8992f04a4"
-netcat -l -p 30000 localhost &
-VBoxManage startvm --putenv VBOX_GUI_DBG_ENABLED=true Test
+VBoxManage startvm --putenv VBOX_GUI_DBG_ENABLED=true Test &
+build/DebugBridge/DebugBridge
