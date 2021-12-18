fe2945b1eb58de6e5560837b87d591a17cd95f5a
Tobias Zagorni <tobias@zagorni.eu>
2021-12-18 07:29:42 +0100

    zagtos++: remove redundant START_CONTROLLER message definition

--- src/PS2Controller/PS2Controller.cpp
+++ src/PS2Controller/PS2Controller.cpp
@@ -2,12 +2,16 @@
 #include <tuple>
 #include <zagtos/Messaging.hpp>
 #include <zagtos/protocols/PS2.hpp>
+#include <zagtos/Interrupt.hpp>
 #include <zagtos/protocols/Controller.hpp>
 
 using namespace zagtos;
 
 int main() {
-    auto envPort = decodeRunMessage<RemotePort>(ps2::MSG_START_CONTROLLER);
-
     std::cout << "Starting PS/2 Controller..." << std::endl;
+
+    using MsgType = std::tuple<RemotePort, std::vector<Interrupt>>;
+    auto [envPort, interrupts] = decodeRunMessage<MsgType>(controller::MSG_START);
+
+    std::cout << "got message" << std::endl;
 }
