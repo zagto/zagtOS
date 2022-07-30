0522a10ee3c65cfb96cd59ec0099d3253cccca6d
Tobias Zagorni <tobias@zagorni.eu>
2022-07-31 01:16:59 +0200

    musl: mix nanosleep call

--- src/musl/src/time/nanosleep.c
+++ src/musl/src/time/nanosleep.c
@@ -3,5 +3,5 @@
 
 int nanosleep(const struct timespec *req, struct timespec *rem)
 {
-    return zagtos_syscall(SYS_CLOCK_NANOSLEEP, CLOCK_MONOTONIC, req);
+    return zagtos_syscall(SYS_CLOCK_NANOSLEEP, 0, CLOCK_MONOTONIC, req);
 }
