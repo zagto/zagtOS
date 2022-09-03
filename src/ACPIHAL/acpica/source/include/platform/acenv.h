beafbfe1dac6be621a9dbc2b0c9d4958c7321c39
Tobias Zagorni <tobias@zagorni.eu>
2022-09-03 23:55:37 +0200

    bring back ACPICA customization

--- src/ACPIHAL/acpica/source/include/platform/acenv.h
+++ src/ACPIHAL/acpica/source/include/platform/acenv.h
@@ -350,6 +350,9 @@
 #elif defined(__QNX__)
 #include "acqnx.h"
 
+#elif defined(__zagtos__)
+#include "aczagtos.h"
+
 /*
  * EFI applications can be built with -nostdlib, in this case, it must be
  * included after including all other host environmental definitions, in
