9e43b75419ae6c3d329f2967893d3c5dcf32483a
Tobias Zagorni <tobias@zagorni.eu>
2022-12-20 12:12:13 +0100

    create-boot-image-aarch64: store uncompressed loader for qemu

--- create-boot-image-aarch64.sh
+++ create-boot-image-aarch64.sh
@@ -2,6 +2,7 @@
 set -e
 
 mkdir -p out/$ARCH
-lz4 --force build/$ARCH/loader/android/vmlinux out/$ARCH/zImage-lz4
+cp build/$ARCH/loader/android/vmlinux out/$ARCH/vmlinux
+lz4 --force out/$ARCH/vmlinux out/$ARCH/zImage-lz4
 cat build/$ARCH/kernel/ZAGTKERN.ZBN build/$ARCH/SystemEnvironment/SYSENV.ZBN > out/$ARCH/initrd.img
 mkbootimg --kernel out/$ARCH/zImage-lz4 --ramdisk out/$ARCH/initrd.img --dtb sunfish.dtb --cmdline 'nothing' --header_version 2 -o boot-sunfish.img
