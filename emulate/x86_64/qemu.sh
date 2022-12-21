4f59dccd7fe481385a0894c51fc59433af442a1e
Tobias Zagorni <tobias@zagorni.eu>
2022-12-21 22:23:09 +0100

    make x86_64 qemu script cross-platform (WIP/untested)

--- emulate/x86_64/qemu.sh
+++ emulate/x86_64/qemu.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 set -e
+
+if [ `uname -s` == "Linux" ]; then
+    ACCEL="-enable-kvm"
+elif [ `uname -s` == "Darwin" ]; then
+    ACCEL="-accel hvf"
+elif which winver; then
+    ACCEL="-accel hax"
+else
+    echo "Unknown operating system, can't figure out Qemu acceleration method"
+    exit 1
+fi
+
 qemu-system-x86_64 \
     -drive if=pflash,format=raw,unit=0,file=/usr/share/ovmf/x64/OVMF_CODE.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=/usr/share/ovmf/x64/OVMF_VARS.fd,readonly=on \
     -m 512 \
     -net none \
-    -enable-kvm \
+    $ACCEL \
     -drive id=disk,file=out/x86_64/disk.img,if=none \
     -device ahci,id=ahci \
     -device ide-hd,drive=disk,bus=ahci.0 \
