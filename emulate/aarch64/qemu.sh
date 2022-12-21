fc2663e723ca310a2ab0e9ba47618bd268664492
Tobias Zagorni <tobias@zagorni.eu>
2022-12-21 17:27:31 +0100

    aarch64 qemu: enable DebugBridge

--- emulate/aarch64/qemu.sh
+++ emulate/aarch64/qemu.sh
@@ -2,15 +2,11 @@
 set -e
 qemu-system-aarch64 \
     -machine virt \
-    -serial stdio \
     -m 512 \
     -cpu cortex-a53 \
     -kernel out/aarch64/vmlinux -initrd out/aarch64/initrd.img \
     -smp 2 \
-    -s
-
-
-#        -serial telnet:localhost:30000,server \
-# -no-reboot -no-shutdown   -d int,guest_errors,cpu_reset \
-#    -s  &
-#build/DebugBridge/DebugBridge
+    -serial telnet:localhost:30000,server \
+    -no-reboot -no-shutdown -d int,guest_errors,cpu_reset \
+    -s &
+build/aarch64/DebugBridge/DebugBridge
